71a651f04218fbb018056e6c6cce96168c6f90bc
Radomirikus <r2a3d0o3m1i2r@gmail.com>
2025-12-04 18:53:18 +0300

    Обьединил принты

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    TOKEN = os.getenv("TOKEN")
+    token = os.getenv("TOKEN")
     url = "https://calendarific.com/api/v2/holidays"
-    payload = {"api_key": TOKEN, "country": "RU", "year": "2025"}
+    payload = {"api_key": token, "country": "RU", "year": "2025"}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     holidays = response.json()['response']['holidays']
     for holiday in holidays:
-        print(f"День: {holiday['date']['datetime']['day']}/{holiday['date']['datetime']['month']}")
-        print("Название праздника: ", holiday['name'])
-        print("Описание: ", holiday['description'], "\n")
+        print(f"""День: {holiday['date']['datetime']['day']}/{holiday['date']['datetime']['month']}
+Название праздника: {holiday['name']}
+Описание: {holiday['description']} \n""")
 
 if __name__ == '__main__':
     main()
